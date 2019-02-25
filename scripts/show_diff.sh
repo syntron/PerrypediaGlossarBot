ae3fbfe7d326774bf449118d7be1fb889957bfb6
Matthias Pfafferodt (syntron) <syntron@web.de>
2019-02-25 19:02:42 +0100

    update script to show diff

--- scripts/show_diff.sh
+++ scripts/show_diff.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
-grep "^\(+\|-\)" ../steps/03diff/*.diff > show_diff.log
+grep "^\(+\|-\)" ../steps/03diff/*.diff				 > show_diff.log
+
+echo -e "\n\nSummary\n"						>> show_diff.log
+
+grep "^\(+\|-\)" ../steps/03diff/*.diff | grep Anzahl		>> show_diff.log
